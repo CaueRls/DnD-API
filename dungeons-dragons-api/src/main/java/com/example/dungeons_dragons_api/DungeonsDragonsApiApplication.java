1823b137657b9352e739826c93d97808df73f8d6
CAUE RODRIGUES LOUREIRO E SILVA <caue.rlesilva@senacsp.edu.br>
2026-03-31 22:21:09 -0300

    first commit

---
+++ dungeons-dragons-api/src/main/java/com/example/dungeons_dragons_api/DungeonsDragonsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dungeons_dragons_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsDragonsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsDragonsApiApplication.class, args);
+	}
+
+}
---
+++ dungeons-dragons-api/src/test/java/com/example/dungeons_dragons_api/DungeonsDragonsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dungeons_dragons_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsDragonsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
